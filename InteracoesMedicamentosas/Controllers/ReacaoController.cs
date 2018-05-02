80171d7f9fb156271541455bb4bb305de0f9ff0f
Diogo Miranda <38512133+Th3Nuk@users.noreply.github.com>
2018-05-02 13:19:52 -0300

    Criacao do Controller

---
+++ InteracoesMedicamentosas/Controllers/ReacaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InteracoesMedicamentosas.Controllers
+{
+    public class ReacaoController : Controller
+    {
+        // GET: Reacao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

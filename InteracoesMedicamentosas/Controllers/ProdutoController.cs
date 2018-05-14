75ed5096583f406686dab5084a7947ce0378bfd1
GustavoRC01 <gustavorc01@hotmail.com>
2018-05-14 20:19:47 -0300

    Index(Produto)Gustavo

--- InteracoesMedicamentosas/Controllers/ProdutoController.cs
+++ InteracoesMedicamentosas/Controllers/ProdutoController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InteracoesMedicamentosas.Context;
+using InteracoesMedicamentosas.Models;
 
 namespace InteracoesMedicamentosas.Controllers
 {
     public class ProdutoController : Controller
     {
+        private EFContext Context = new EFContext();
         // GET: Produto
         public ActionResult Index()
         {
-            return View();
+            List<Produto> ListaProdutos = Context.Produtos.ToList();
+            return View(ListaProdutos);
         }
+
     }
 }

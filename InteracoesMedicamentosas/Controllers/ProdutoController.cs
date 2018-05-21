3b3f8f55346ab6ce43667ee341152f075af46608
GustavoRC01 <gustavorc01@hotmail.com>
2018-05-21 20:53:42 -0300

    View Edit e modificações no controllerProduto

--- InteracoesMedicamentosas/Controllers/ProdutoController.cs
+++ InteracoesMedicamentosas/Controllers/ProdutoController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using InteracoesMedicamentosas.Context;
@@ -16,6 +18,33 @@ namespace InteracoesMedicamentosas.Controllers
         {
             List<Produto> ListaProdutos = Context.Produtos.ToList();
             return View(ListaProdutos);
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = Context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Entry(produto).State = EntityState.Modified;
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
         }
 
     }
